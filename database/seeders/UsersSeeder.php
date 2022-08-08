30c7e48b97bf88093eb45175f53c0737ce0744b4
Luis Andrés <luisandres33bolanos@gmail.com>
2022-08-08 18:39:26 +0200

    seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::updateOrcreate([
+            'email' => 'luisandres33bolanos@gmail.com',
+            'password' => Hash::make('123456789')
+        ]);
+    }
+}
