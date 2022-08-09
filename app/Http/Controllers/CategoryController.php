2137578155dc5a881cf9b0c180ba985f3b85ac88
Luis Andrés <luisandres33bolanos@gmail.com>
2022-08-09 20:17:42 +0200

    controlador

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([Category::fromContext()], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = $request->only(Category::UPDATE_FIELDS);
+        $data['created_by'] = intval(Auth::user()->id);
+        $category = Category::create($data);
+
+        return response()->json(['data' => $category], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        if (Auth::user()->cannot('update', $category)) {
+            return response()->json([], Response::HTTP_UNAUTHORIZED);
+        }
+        $data = $request->only(Category::UPDATE_FIELDS);
+        $category->update($data);
+        return response()->json(['data' => $category], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        if (Auth::user()->cannot('delete', $category)) {
+            return response()->json([], Response::HTTP_UNAUTHORIZED);
+        }
+        $category->delete();
+        return response()->json(['data' => $category], Response::HTTP_OK);
+    }
+}
